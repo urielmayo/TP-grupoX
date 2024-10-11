254fe4aa6294f40c07c697a96d573439132dd259
Octavio Ramírez <octavio.ramirez.3980@gmail.com>
2024-10-11 18:02:08 -0300

    Se agrega repositorio generico de técnicos

---
+++ TPDDSBackend/Infrastructure/Repositories/TechnicianRepository.cs
@@ -0,0 +1,12 @@
+﻿using TPDDSBackend.Domain.EF.DBContexts;
+using TPDDSBackend.Domain.Entitites;
+
+namespace TPDDSBackend.Infrastructure.Repositories
+{
+    public class TechnicianRepository : GenericRepository<Technician>, IGenericRepository<Technician>
+    {
+        public TechnicianRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
