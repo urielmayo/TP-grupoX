fb42ffccc1d30113223f8e53a70a8a8cf3a703f2
DiegoGonzalezFerrer <dgonzlezferrer@frba.utn.edu.ar>
2024-11-21 22:04:53 -0300

    added endpoint to suggest fridge locations (#21)
    Co-authored-by: Diego Gonzalez <alfonzoferrer97@gmail.com>

---
+++ TPDDSBackend/Aplication/Dtos/Requests/SuggestFridgeLocationsRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace TPDDSBackend.Aplication.Dtos.Requests
+{
+    public class SuggestFridgeLocationsRequest
+    {
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public int RadiusInKm { get; set; }
+
+        public int NumberOfPoints { get; set; }
+    }
+}
---
+++ TPDDSBackend/Aplication/Dtos/Responses/SuggestFridgeLocationsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TPDDSBackend.Aplication.Dtos.Responses
+{
+    public class SuggestFridgeLocationsResponse
+    {
+        public List<FridgeLocationsResponse> Locations { get; set; }
+        public SuggestFridgeLocationsResponse(List<FridgeLocationsResponse> locations)
+        {
+            Locations = locations;
+        }
+    }
+}
