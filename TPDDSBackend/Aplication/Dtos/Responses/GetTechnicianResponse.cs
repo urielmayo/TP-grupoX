17928f2fc26fb37e6ffc6437073d8464fd5f176a
DiegoGonzalezFerrer <dgonzlezferrer@frba.utn.edu.ar>
2024-12-03 23:57:35 -0300

    Backend (#27)
    * added endpoint to suggest fridge locations (#21)
    
    Co-authored-by: Diego Gonzalez <alfonzoferrer97@gmail.com>
    
    * change public access get fridges and register temperature
    
    * added documentType mapped
    
    * added mapper get technician
    
    * fix map technician
    
    ---------
    
    Co-authored-by: Diego Gonzalez <alfonzoferrer97@gmail.com>

--- TPDDSBackend/Aplication/Dtos/Responses/GetTechnicianResponse.cs
+++ TPDDSBackend/Aplication/Dtos/Responses/GetTechnicianResponse.cs
@@ -7,11 +7,11 @@ namespace TPDDSBackend.Aplication.Dtos.Responses
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public int IdDocumentType { get; set; }
+        public string DocumentTypeName { get; set; }
         public string IdNumber { get; set; }
         public string WorkerIdentificationNumber { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
-        public string Neighbourhood { get; set; }
+        public string NeighbourhoodName { get; set; }
     }
 }
