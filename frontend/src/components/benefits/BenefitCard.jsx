da93846dd536d174160353101386b0552f11e797
urielmayo <urimayo00@gmail.com>
2024-12-12 12:22:16 -0300

    agregando funcionalidad de canjear productos

--- frontend/src/components/benefits/BenefitCard.jsx
+++ frontend/src/components/benefits/BenefitCard.jsx
@@ -1,28 +1,75 @@
 /* eslint-disable react/prop-types */
-export default function BenefitCard({ product }) {
+import { config } from "../../config";
+import { replace, useNavigate } from "react-router-dom";
+
+export default function BenefitCard({ benefit }) {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    try {
+      const jwt = sessionStorage.getItem("jwt"); // Obtiene el token JWT desde el sessionStorage
+      if (!jwt) {
+        alert("Error: No se encontró un token de autenticación.");
+        return;
+      }
+
+      const response = await fetch(
+        `${config.BACKEND_URL}/Collaborator/exchange-benefit`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${jwt}`, // Incluye el JWT en el header
+          },
+          body: JSON.stringify({ benefitId: benefit.id }), // Construye el cuerpo del POST
+        }
+      );
+
+      if (!response.ok) {
+        // Manejo de errores de respuesta
+        const errorData = await response.json();
+        alert(`Error: ${errorData.message || "No se pudo realizar el canje."}`);
+        return;
+      }
+
+      // Si la respuesta fue exitosa
+      alert("Producto canjeado exitosamente.");
+      navigate(".", { replace: true });
+    } catch (error) {
+      // Manejo de errores de conexión o fetch
+      alert(
+        `Error: ${
+          error.message || "Ocurrió un problema al intentar realizar el canje."
+        }`
+      );
+    }
+  };
+
   return (
-    <div className="max-w-sm overflow-hidden rounded-2xl shadow-lg bg-white">
-      <img className="object-scale-down" src={product.Imagen} alt="" />
-      <hr />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{product.Descripcion}</div>
+    <div className="flex w-full overflow-hidden rounded-2xl shadow-lg bg-white">
+      <div className="flex-1 px-6 py-4">
+        <div className="font-bold text-xl mb-2">{benefit.description}</div>
         <p className="text-gray-700 text-base mb-2">
-          <strong>Rubro:</strong> {product.Rubro}
+          <strong>Rubro:</strong> {benefit.category}
         </p>
         <p className="text-gray-700 text-base mb-2">
-          <strong>Puntos necesarios:</strong> {product.Puntos}
+          <strong>Puntos necesarios:</strong> {benefit.requiredPoints}
         </p>
         <p className="text-gray-700 text-base mb-2">
-          <strong>Empresa:</strong> {product.Empresa}
+          <strong>Empresa:</strong> {benefit.Empresa}
         </p>
-        <br />
-        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-600 mr-2 mb-2">
-          {product.Rubro}
-        </span>
-        <br />
-        <button className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+        <button
+          onClick={handleClick}
+          className="bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-white mt-2"
+        >
           Canjear
         </button>
+      </div>
+      <div className="flex-shrink-0">
+        <img
+          className="object-contain h-full w-48"
+          src={benefit.imagePath}
+          alt="Imagen del beneficio"
+        />
       </div>
     </div>
   );
--- frontend/src/components/benefits/BenefitList.jsx
+++ frontend/src/components/benefits/BenefitList.jsx
@@ -4,20 +4,21 @@ import GridLayout from "../../layouts/GridLayout";
 import EmptyGrid from "../UI/EmptyGrid";
 
 export default function BenefitList() {
-  const { user, rewards } = useLoaderData();
+  const { user, benefits } = useLoaderData();
+
   return (
     <GridLayout
       header={
         <>
           <h1 className="text-4xl">Mis contribuciones</h1>
           <div className="min-h-10 border-t sm:border-t-0 sm:border-s border-gray-200 dark:border-neutral-700"></div>
-          <h1 className="text-2xl">
+          <h1 className="text-2xl mt-2">
             Puntos acumulados: {user.accumulatedPoints}
           </h1>
         </>
       }
-      items={rewards}
-      renderItem={(reward) => <BenefitCard key={reward.id} product={reward} />}
+      items={benefits}
+      renderItem={(benfit) => <BenefitCard key={benfit.id} benefit={benfit} />}
       emptyItem={<EmptyGrid title="No hay productos" />}
     />
   );
