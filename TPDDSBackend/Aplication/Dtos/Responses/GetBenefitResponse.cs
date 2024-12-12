f1573439f17f430341eb3550b9856df22e84c066
Diego Gonzalez <alfonzoferrer97@gmail.com>
2024-12-11 22:28:51 -0300

    Added endpoint to get all benefits

---
+++ TPDDSBackend/Aplication/Dtos/Responses/GetBenefitResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TPDDSBackend.Aplication.Dtos.Responses
+{
+    public class GetBenefitResponse
+    {
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public int RequiredPoints { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string CollaboratorId { get; set; }
+    }
+}
