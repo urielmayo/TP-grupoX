3cdadd154d61d195516b3c7118c83027c2ec8810
urielmayo <urimayo00@gmail.com>
2024-12-12 23:19:54 -0300

    cambio estilos de heladera

--- frontend/src/components/fridges/FridgeDetail.jsx
+++ frontend/src/components/fridges/FridgeDetail.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 export default function FridgeDetail() {
   const fridge = useLoaderData();
@@ -7,12 +7,13 @@ export default function FridgeDetail() {
 
   return (
     <div className="bg-white shadow-lg rounded-2xl p-8 min-w-full">
-      <div className="flex">
-        <div className="flex-1 mr-5">
+      <div className="flex gap-x-10">
+        <div className="flex-1">
           <h1 className="text-4xl">{fridge.name}</h1>
-          <hr />
+          <hr className="my-4" />
           <div>
             <address className="text-xl">{fridge.address}</address>
+            <p className="mt-2">Capacidad maxima: {fridge.maxFoodCapacity}</p>
           </div>
         </div>
         <div className="flex-1">
