a065603a6715284460480ec1068d943f3c9d6b32
urielmayo <urimayo00@gmail.com>
2024-12-16 23:42:09 -0300

    agregando incidentes a vista de heladera

--- frontend/src/components/fridges/FridgeDetail.jsx
+++ frontend/src/components/fridges/FridgeDetail.jsx
@@ -1,12 +1,20 @@
 import { useLoaderData, Link } from "react-router-dom";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { getUserData } from "../../utils/auth";
+import IncidentsTable from "./IncidentsTable";
 
 export default function FridgeDetail() {
-  const fridge = useLoaderData();
+  const {
+    name,
+    address,
+    maxFoodCapacity,
+    foodCapacityAvailable,
+    latitud,
+    longitud,
+    lastFridgeIncidents,
+  } = useLoaderData();
   const { role } = getUserData();
-  console.log(fridge);
-  console.log(role);
+  console.log(lastFridgeIncidents);
 
   return (
     <div>
@@ -17,7 +25,7 @@ export default function FridgeDetail() {
         <div className="flex gap-x-10">
           <div className="flex-1">
             <div className="flex justify-between items-baseline">
-              <h1 className="text-4xl">{fridge.name}</h1>
+              <h1 className="text-4xl">{name}</h1>
               {role === "Admin" && (
                 <Link
                   to="visit"
@@ -29,11 +37,23 @@ export default function FridgeDetail() {
             </div>
             <hr className="my-4" />
             <div>
-              <address className="text-xl">{fridge.address}</address>
-              <p className="mt-2">Capacidad maxima: {fridge.maxFoodCapacity}</p>
+              <address className="text-xl">{address}</address>
+              <p className="mt-2">Capacidad maxima: {maxFoodCapacity}</p>
+              <p className="mt-2">
+                Lugares disponibles: {foodCapacityAvailable}
+              </p>
             </div>
             <br />
-            <h1 className="text-2xl">Avisarme cuando ...</h1>
+            <h1 className="text-xl mb-2">Ultimos incidentes reportados</h1>
+            {(lastFridgeIncidents.length && (
+              <IncidentsTable incidents={lastFridgeIncidents} />
+            )) || (
+              <p className="font-thin text-sm">
+                Esta heladera no tiene incidentes reportados
+              </p>
+            )}
+            <br />
+            <h1 className="text-xl">Avisarme cuando ...</h1>
             <ul className="list-disc list-inside mt-3">
               <li className="mb-2">
                 Queden{" "}
@@ -70,17 +90,17 @@ export default function FridgeDetail() {
             </ul>
           </div>
 
-          <div className="flex-1">
+          <div className="flex-shrink-0">
             <MapContainer
-              center={[fridge.latitud, fridge.longitud]}
+              center={[latitud, longitud]}
               zoom={15}
-              style={{ height: "500px", width: "100%" }}
+              style={{ height: "100%", width: "500px" }}
             >
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               />
-              <Marker position={[fridge.latitud, fridge.longitud]}></Marker>
+              <Marker position={[latitud, longitud]}></Marker>
             </MapContainer>
           </div>
         </div>
