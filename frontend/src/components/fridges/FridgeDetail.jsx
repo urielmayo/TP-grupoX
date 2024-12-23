543151f5f7ae9a9dca7c8d8f766248103e82a3f1
urielmayo <urimayo00@gmail.com>
2024-12-23 00:24:27 -0300

    agregar campos nuevos a vista de detalle de heladera

--- frontend/src/components/fridges/FridgeDetail.jsx
+++ frontend/src/components/fridges/FridgeDetail.jsx
@@ -17,8 +17,12 @@ export default function FridgeDetail() {
     latitud,
     longitud,
     lastFridgeIncidents,
+    active,
+    setUpAt,
+    currentTemperature,
   } = useLoaderData();
   const { role } = getUserData();
+  const setUpDate = new Date(setUpAt);
 
   return (
     <div>
@@ -30,29 +34,43 @@ export default function FridgeDetail() {
           <div className="flex-1">
             <div className="flex justify-between items-baseline">
               <h1 className="text-4xl">{name}</h1>
-              {role === "Admin" && (
-                <Link
-                  to="visit"
-                  className="bg-gray-800 hover:bg-gray-700 px-2 py-1 text-white text-xl rounded-lg"
-                >
-                  Programar visita
-                </Link>
-              )}
-              {role === "Collaborator" && (
-                <Link
-                  to="incident"
-                  className="bg-red-500 hover:bg-red-600 px-2 py-1 text-white text-xl rounded-lg"
-                >
-                  Reportar incidente
-                </Link>
-              )}
+              {(!active && (
+                <div className="mb-2">
+                  <span className="px-2 py-1 bg-auto bg-red-300 rounded-md text-xl font-bold">
+                    Esta heladera se encuentra inactiva
+                  </span>
+                </div>
+              )) ||
+                (role === "Admin" && (
+                  <Link
+                    to="visit"
+                    className="bg-gray-800 hover:bg-gray-700 px-2 py-1 text-white text-xl rounded-lg"
+                  >
+                    Programar visita
+                  </Link>
+                )) ||
+                (role === "Collaborator" && (
+                  <Link
+                    to="incident"
+                    className="bg-red-500 hover:bg-red-600 px-2 py-1 text-white text-xl rounded-lg"
+                  >
+                    Reportar incidente
+                  </Link>
+                ))}
             </div>
             <hr className="my-4" />
+
             <div>
               <address className="text-xl">{address}</address>
-              <p className="mt-2">Capacidad maxima: {maxFoodCapacity}</p>
               <p className="mt-2">
-                Lugares disponibles: {foodCapacityAvailable}
+                Puesta en funcionamiento:{" "}
+                {setUpDate.toLocaleDateString("es-AR")}
+              </p>
+              <p className="mt-2">
+                Espacios disponibles: {foodCapacityAvailable}/{maxFoodCapacity}
+              </p>
+              <p className="mt-2">
+                Ultima temperatura registrada: {currentTemperature}° C
               </p>
             </div>
             <br />
@@ -65,35 +83,39 @@ export default function FridgeDetail() {
               </p>
             )}
             <br />
-            <h1 className="text-xl">Avisarme cuando ...</h1>
-            <ul className="list-disc list-inside mt-3">
-              <li className="mb-2">
-                Queden{" "}
-                <input
-                  className="border rounded-sm max-w-10"
-                  type="number"
-                  name=""
-                  id=""
-                />{" "}
-                viandas para que la heladera quede vacia
-                <button className="ml-10 px-2 py-1 bg-blue-600 rounded-md text-white">
-                  Avisame
-                </button>
-              </li>
-              <li className="mb-2">
-                Queden{" "}
-                <input
-                  className="border rounded-sm max-w-10"
-                  type="number"
-                  name=""
-                  id=""
-                />{" "}
-                viandas para que la heladera se llene
-                <button className="ml-10 px-2 py-1 bg-blue-600 rounded-md text-white">
-                  Avisame
-                </button>
-              </li>
-            </ul>
+            {active && (
+              <>
+                <h1 className="text-xl">Avisarme cuando ...</h1>
+                <ul className="list-disc list-inside mt-3">
+                  <li className="mb-2">
+                    Queden{" "}
+                    <input
+                      className="border rounded-sm max-w-10"
+                      type="number"
+                      name=""
+                      id=""
+                    />{" "}
+                    viandas para que la heladera quede vacia
+                    <button className="ml-10 px-2 py-1 bg-blue-600 rounded-md text-white">
+                      Avisame
+                    </button>
+                  </li>
+                  <li className="mb-2">
+                    Queden{" "}
+                    <input
+                      className="border rounded-sm max-w-10"
+                      type="number"
+                      name=""
+                      id=""
+                    />{" "}
+                    viandas para que la heladera se llene
+                    <button className="ml-10 px-2 py-1 bg-blue-600 rounded-md text-white">
+                      Avisame
+                    </button>
+                  </li>
+                </ul>
+              </>
+            )}
           </div>
 
           <div className="flex-shrink-0">
