25601fb09e2c0ce64b705091c0a034fdfd526474
urielmayo <urimayo00@gmail.com>
2024-12-25 17:07:07 -0300

    cambios de estilos

--- frontend/src/components/UI/EmptyGrid.jsx
+++ frontend/src/components/UI/EmptyGrid.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function EmptyGrid({ title, text }) {
   return (
-    <div className="bg-gray-200 rounded-2xl p-8 shadow-md min-h-72">
+    <div className="bg-gray-200 rounded-2xl p-8 min-h-72">
       <h1 className="font-bold text-2xl mb-2">{title}</h1>
       {text && (
         <p>
