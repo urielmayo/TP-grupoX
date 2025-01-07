227660f8b61c8c7733bdd0e6a4350c21922b4732
urielmayo <urimayo00@gmail.com>
2025-01-07 09:51:22 -0300

    ajustando altura minima para tarjetas

--- frontend/src/components/UI/Card.jsx
+++ frontend/src/components/UI/Card.jsx
@@ -2,7 +2,7 @@
 
 export default function Card({ children }) {
   return (
-    <div className=" overflow-hidden rounded-2xl shadow-sm hover:shadow-lg hover:shadow-gray-500/50 p-4 bg-white group">
+    <div className=" overflow-hidden rounded-2xl shadow-sm hover:shadow-lg hover:shadow-gray-500/50 p-4 bg-white group min-h-36">
       {children}
     </div>
   );
