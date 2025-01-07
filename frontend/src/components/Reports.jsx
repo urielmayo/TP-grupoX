3a237d096b284f89a5f4e24e41145a8b919c7183
urielmayo <urimayo00@gmail.com>
2025-01-06 23:24:41 -0300

    se genera descarga de reporte

--- frontend/src/components/Reports.jsx
+++ frontend/src/components/Reports.jsx
@@ -3,6 +3,7 @@ import FormTitle from "./UI/form/FormTitle";
 import Field from "./UI/form/Field";
 import SubmitButton from "./UI/form/SubmitButton";
 import RadioGroup from "./UI/form/RadioGroup";
+import { Form } from "react-router-dom";
 
 export default function Reports() {
   // Calculate default dates
@@ -14,42 +15,44 @@ export default function Reports() {
   const formatDate = (date) => date.toISOString().split("T")[0];
 
   return (
-    <FormLayout>
-      <FormTitle text="Solicitar reporte" />
-      <br />
-      <div className="grid grid-cols-2 gap-x-4">
-        <Field
-          label="Fecha de inicio"
-          type="date"
-          name="startDate"
-          defaultValue={formatDate(lastWeek)}
+    <Form method="post">
+      <FormLayout>
+        <FormTitle text="Solicitar reporte" />
+        <br />
+        <div className="grid grid-cols-2 gap-x-4">
+          <Field
+            label="Fecha de inicio"
+            type="date"
+            name="fromDate"
+            defaultValue={formatDate(lastWeek)}
+          />
+          <Field
+            label="Fecha de fin"
+            type="date"
+            name="toDate"
+            defaultValue={formatDate(today)}
+          />
+        </div>
+        <RadioGroup
+          label="Tipo de reporte"
+          name="reportType"
+          options={[
+            {
+              value: "fridge-failures",
+              label: "Cantidad de Fallas por Heladera",
+            },
+            {
+              value: "fridge-movements",
+              label: "Cantidad de Viandas Retiradas/Colocadas por Heladera",
+            },
+            {
+              value: "foods-by-collaborator",
+              label: "Cantidad de Viandas por colaborador",
+            },
+          ]}
         />
-        <Field
-          label="Fecha de fin"
-          type="date"
-          name="endDate"
-          defaultValue={formatDate(today)}
-        />
-      </div>
-      <RadioGroup
-        label="Tipo de reporte"
-        name="reportType"
-        options={[
-          {
-            value: "food",
-            label: "Cantidad de Fallas por Heladera",
-          },
-          {
-            value: "non-food",
-            label: "Cantidad de Viandas Retiradas/Colocadas por Heladera",
-          },
-          {
-            value: "all",
-            label: "Cantidad de Viandas por colaborador",
-          },
-        ]}
-      />
-      <SubmitButton text="Solicitar" />
-    </FormLayout>
+        <SubmitButton text="Solicitar" />
+      </FormLayout>
+    </Form>
   );
 }
